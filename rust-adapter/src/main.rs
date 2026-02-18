d5b7a6d2e55cb0066674c11cb3299fefc65a59e2
Sneha <sssnehasinha787@gmail.com>
2026-02-18 11:05:52 +0530

    adding python to rust

--- rust-adapter/src/main.rs
+++ rust-adapter/src/main.rs
@@ -12,6 +12,9 @@ use api::start_server;
 // #[warn(unused_imports)]
 // use crate::api::CheckAccessRequest;
 
+use pyo3::types::PyModule;
+use pyo3::prelude::*;
+
 #[tokio::main]
 // async fn check_access(
 //     Json(req): Json<CheckAccessRequest>,
@@ -35,10 +38,26 @@ async fn main() {
     println!("PLC Port: {} @ {} baud", cfg.plc_port, cfg.plc_baudrate);
     println!("HTTP Server: {}:{}", cfg.server_host, cfg.server_port);
 
+    tokio::task::spawn_blocking(||{
+        if let Err(e)= python(){
+            eprintln!("Python error: {:?}",e);
+        }
+    });
     // Initialize PLC
     let plc = KeyencePlc::new(&cfg);
     // Start HTTP server (this blocks)
     if let Err(e) = start_server(cfg, plc).await {
         eprintln!("Server error: {}", e);
     }
+}
+
+
+fn python() -> PyResult<()> {
+    pyo3::Python::initialize();
+
+    Python::attach(|py| {
+        let module = PyModule::import(py, "main")?;
+        module.getattr("main")?.call0()?;
+        Ok(())
+    })
 }
